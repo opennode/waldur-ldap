63469b122a6b63e01cbcf432fa40f87502ed1ba7
Ilja Livenson <ilja@opennodecloud.com>
2017-05-02 23:27:53 +0300

    Fix migration
    - WAL-758

--- src/nodeconductor_ldap/migrations/0001_initial.py
+++ src/nodeconductor_ldap/migrations/0001_initial.py
@@ -75,8 +75,8 @@ class Migration(migrations.Migration):
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'verbose_name': 'LDAP User',
-                'verbose_name_plural': 'LDAP User',
+                'verbose_name': 'LDAP user',
+                'verbose_name_plural': 'LDAP user',
             },
             bases=(nodeconductor.core.models.DescendantMixin, nodeconductor.core.models.BackendModelMixin, nodeconductor.logging.loggers.LoggableMixin, models.Model),
         ),
